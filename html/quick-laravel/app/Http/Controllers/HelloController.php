56f227e9eef32ea957639276ec9df59bf19816c0
fjsho <gorimura7@gmail.com>
2021-07-05 21:18:34 +0900

    [add]3.2 viewの基本

--- html/quick-laravel/app/Http/Controllers/HelloController.php
+++ html/quick-laravel/app/Http/Controllers/HelloController.php
@@ -12,4 +12,15 @@ class HelloController extends Controller
         //c.出力を戻り値に
         return'こんにちは、世界！';
     }
+
+    public function view()
+    {
+        //a,ビュー変数を準備
+        $data = [
+            'msg' => 'こんにちは、世界！'
+        ];
+        //b.テンプレートを呼び出す
+        return view('hello.view',$data);
+    }
 }
+
--- html/quick-laravel/routes/web.php
+++ html/quick-laravel/routes/web.php
@@ -11,10 +11,21 @@
 |
 */
 
-use Illuminate\Routing\Route;
+//下記の一行は最初から入っていたが、これを有効にするとエラーが出るので注意。原因不明っぽいので講師の方に相談してもいいかも？https://python-konbu.hateblo.jp/entry/2020/10/24/034036（参考）
+//use Illuminate\Routing\Route;
+
 //「/」に対するルート
 Route::get('/', function () {
     return view('welcome');
 });
 //「/hello」に対するルート
 Route::get('hello','HelloController@index');
+    /*
+    /   functionで同じことを行うルート
+    /   Route::get('hello1',function(){
+    /   return 'hello world!!';
+    /   });
+    */
+
+//「/hello/view」に対するルート
+Route::get('hello/view', 'HelloController@view');
